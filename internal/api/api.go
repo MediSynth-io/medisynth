db408a32e053e6a3a9c33609f7cb338e5b4e3964
Ethan <maker2413@gmail.com>
2025-06-11 02:03:37 -0500

    deduplicating ai halucinations (#2)

--- internal/api/api.go
+++ internal/api/api.go
@@ -3,10 +3,12 @@ package api
 import (
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/MediSynth-io/medisynth/internal/config"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type Api struct {
@@ -14,7 +16,7 @@ type Api struct {
 }
 
 func NewApi(config config.Config) (*Api, error) {
-	if config.ApiPort == 0 {
+	if config.APIPort == 0 {
 		return nil, errors.New("Must have at least a port to start API")
 	}
 
@@ -28,9 +30,24 @@ func NewApi(config config.Config) (*Api, error) {
 func (api *Api) Serve() {
 	r := chi.NewRouter()
 
+	// Middleware
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello"))
 	})
 
-	http.ListenAndServe(fmt.Sprintf(":%d", api.Config.ApiPort), r)
+	// Routes
+	r.Get("/heartbeat", api.Heartbeat)
+	r.Post("/generate-patients", api.RunSyntheaGeneration)
+
+	log.Printf("Starting server on port %d...", api.Config.APIPort)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", api.Config.APIPort), r); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+
+	http.ListenAndServe(fmt.Sprintf(":%d", api.Config.APIPort), r)
 }
